754dbcf556d6ea7044297b67b87811d818c9ff66
Nikita Titov <nmtitov@nmtitov.com>
2016-02-08 15:09:03 +0200

    Initial Commit

---
+++ DoubleClick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoubleClick
+//
+//  Created by Nikita on 08/02/16.
+//  Copyright © 2016 Nikita Mikhailovich Titov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DoubleClick/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoubleClick
+//
+//  Created by Nikita on 08/02/16.
+//  Copyright © 2016 Nikita Mikhailovich Titov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
