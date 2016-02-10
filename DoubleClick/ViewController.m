934839edcdee3a8ef011d6ec00d40d771f466ea5
Nikita Titov <nmtitov@nmtitov.com>
2016-02-10 22:01:48 +0200

    New implementation!

--- DoubleClick/ViewController.m
+++ DoubleClick/ViewController.m
@@ -7,8 +7,31 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa.h>
+
+@interface NSNumber (NT)
+
+- (instancetype)numberByAddingOne__nt;
+- (instancetype)numberByAddingNumber__nt:(NSNumber *)r;
+
+@end
+
+@implementation NSNumber (NT)
+
+- (instancetype)numberByAddingOne__nt {
+    return [self numberByAddingNumber__nt:@1];
+}
+
+- (instancetype)numberByAddingNumber__nt:(NSNumber *)r {
+    return @(self.doubleValue + r.doubleValue);
+}
+
+@end
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -16,12 +39,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    RAC(self.label, text) = [[[[
+    [self.button rac_signalForControlEvents:UIControlEventTouchUpInside]
+    scanWithStart:@0 reduce:^id(NSNumber *x, id _) {
+        return [x numberByAddingOne__nt];
+    }]
+    map:^id(id x) {
+        return [NSString stringWithFormat:@"Clicks: %@", x];
+    }]
+    takeUntil:[[self.button rac_signalForControlEvents:UIControlEventTouchUpInside] throttle:0.25]]
+    repeat];
 }
 
 @end
---
+++ Pods/Target Support Files/Pods-DoubleClick/Pods-DoubleClick-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DoubleClick : NSObject
+@end
+@implementation PodsDummy_Pods_DoubleClick
+@end
