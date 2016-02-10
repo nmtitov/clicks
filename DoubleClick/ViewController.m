1b564a00906855af499ba622394e5d5f1127b4ad
Nikita Titov <nmtitov@nmtitov.com>
2016-02-10 22:19:14 +0200

    Revert "lift"
    This reverts commit e33e2532b7e6acca6d54e83944aac93477921477.

--- DoubleClick/ViewController.m
+++ DoubleClick/ViewController.m
@@ -21,7 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    id x = [[[[
+    RAC(self.label, text) = [[[[
     [self.button rac_signalForControlEvents:UIControlEventTouchUpInside]
     scanWithStart:[NSArray new] reduce:^id(NSArray *xs, id x) {
         return [xs arrayByAddingObject:x];
@@ -31,8 +31,6 @@
     }]
     takeUntil:[[self.button rac_signalForControlEvents:UIControlEventTouchUpInside] throttle:0.25]]
     repeat];
-    
-    [self.label rac_liftSelector:@selector(setText:) withSignals:x, nil];
 }
 
 @end
